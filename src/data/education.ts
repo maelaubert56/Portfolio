acf07fcf80517c4b3d48d8b263549e840525baa0
Maël AUBERT <maelaubert56@gmail.com>
2026-07-10 19:45:58 +0200

    fix images

--- src/data/education.ts
+++ src/data/education.ts
@@ -6,13 +6,13 @@ export const education: Education[] = [
     school: "EFREI Paris Panthéon Assas",
     dateStart: "2021-09",
     dateEnd: "2026-08",
-    icon: "logo-efrei.png",
+    icon: "logo-efrei.webp",
   },
   {
     id: "apu",
     school: "Asia Pacific University",
     dateStart: "2023-08",
     dateEnd: "2023-12",
-    icon: "logo-apu.png",
+    icon: "logo-apu.webp",
   },
 ];
